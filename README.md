36d1313ab3938afd1d7a25b4f28b631824f87587
Lokno <lokno1@gmail.com>
2025-09-28 22:39:56 -0400

    Initial commit of v0.1.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# PEAKTrash
+
+Ever wished you could actually put things in the trash and recycling bins in the airport? No? Well, ok, dang.
+
+Replaces the single BoxCollider on trash bins with 4 BoxColliders.
+
+## Installation
+
+Every scout needs to install this mod for it to work properly 
+
+### Manual Installation
+
+1.) Download BepInEx (https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.3/BepInEx_win_x64_5.4.23.3.zip)
+    Download and compile this mod (Precompiled dll coming soon)
+
+2.) Extract the contents of the zip archive into the PEAK game directory (default: "C:\Program Files (x86)\Steam\steamapps\common\PEAK")
+
+3.) Start the game and close it again. This will do the first time set up for Bepinex.
+
+4.) Download the mod from Thunderstore (https://thunderstore.io/c/peak/p/Lokno/PEAKTrash/)
+
+5.) Unzip the zip archive into ...\PEAK\BepInEx\plugins
+
+6.) Run the game
+
---
+++ src/PEAKTrash/Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PEAKTrash;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+    string prevSceneName = string.Empty;
+    const float binThickness = 0.1f;
+
+    private void Awake()
+    {
+        Log = Logger;
+        Log.LogInfo($"Plugin {Name} is loaded! Better Airport Trash Bin Collision");
+    }
+
+    private void ReplaceTrashColliders(GameObject obj)
+    {
+        BoxCollider collider = obj.GetComponent<BoxCollider>();
+
+        if (collider != null)
+        {
+            Vector3 center = collider.center;
+            Vector3 size = collider.size;
+
+            Object.Destroy(collider);
+
+            // Front
+            BoxCollider frontCollider = obj.AddComponent<BoxCollider>();
+            frontCollider.center = new Vector3(center.x, center.y + (size.y - binThickness) * 0.5f, center.z);
+            frontCollider.size = new Vector3(size.x, binThickness, size.z);
+
+            // Back
+            BoxCollider backCollider = obj.AddComponent<BoxCollider>();
+            backCollider.center = new Vector3(center.x, center.y - (size.y - binThickness) * 0.5f, center.z);
+            backCollider.size = new Vector3(size.x, binThickness, size.z);
+
+            // Left
+            BoxCollider leftCollider = obj.AddComponent<BoxCollider>();
+            leftCollider.center = new Vector3(center.x - (size.x - binThickness) * 0.5f, center.y, center.z);
+            leftCollider.size = new Vector3(binThickness, size.y - binThickness * 2, size.z);
+
+            // Right
+            BoxCollider rightCollider = obj.AddComponent<BoxCollider>();
+            rightCollider.center = new Vector3(center.x + (size.x - binThickness) * 0.5f, center.y, center.z);
+            rightCollider.size = new Vector3(binThickness, size.y - binThickness * 2, size.z);
+        }
+    }
+
+    private void Update()
+    {
+        Scene activeScene = SceneManager.GetActiveScene();
+        string name = activeScene.name;
+
+        if (name != prevSceneName)
+        {
+            prevSceneName = name;
+
+            if ("Airport" == name)
+            {
+                foreach (GameObject obj in FindObjectsByType<GameObject>(FindObjectsSortMode.None))
+                {
+                    if (obj.name.StartsWith("Trash"))
+                    {
+                        ReplaceTrashColliders(obj);
+                    }
+                }
+            }
+        }
+    }
+}
